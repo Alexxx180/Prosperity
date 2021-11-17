dd323037511693135a2b39fbe06603dcfb5d5ef6
Alexxx180 <alexx120702@gmail.com>
2021-11-18 00:16:38 +0300

    Fixed creating procedures due to limiter

--- CreateProcedures.sql
+++ CreateProcedures.sql
@@ -1,3 +1,5 @@
+delimiter \;
+
 -- Tables with no relation
 -- to other smaller tables
 
@@ -10,13 +12,12 @@ CREATE PROCEDURE add_level(
 BEGIN
 	INSERT INTO levels(`Name`, `Description`)
 	VALUES (name, description);
-END
+END;
 
 CREATE PROCEDURE get_level(id MEDIUMINT)
 BEGIN
 	SELECT * FROM levels	WHERE `ID` = id;
-END
-
+END;
 
 CREATE PROCEDURE set_level(
 	id MEDIUMINT,
@@ -29,17 +30,17 @@ BEGIN
 	`Name` = name,
 	`Description` = description
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_level(id MEDIUMINT)
 BEGIN
 	DELETE FROM levels WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_levels()
 BEGIN
 	DELETE FROM levels;
-END
+END;
 
 
 -- table: hours
@@ -57,12 +58,12 @@ BEGIN
 	`Lections`, `Practice`, `LabWorks`, `Prepare`)
 	VALUES (course, control, lections,
 	practice, lab_works, prepare_works);
-END
+END;
 
 CREATE PROCEDURE get_hour(id MEDIUMINT)
 BEGIN
 	SELECT * FROM hours WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_hour(
 		id MEDIUMINT,
@@ -83,17 +84,17 @@ BEGIN
 	`LabWorks` = lab_works,
 	`Prepare` = prepare_works
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_hour(id MEDIUMINT)
 BEGIN
 	DELETE FROM hours WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_hours()
 BEGIN
 	DELETE FROM hours;
-END
+END;
 
 
 -- table: discipline_codes
@@ -102,12 +103,12 @@ CREATE PROCEDURE add_discipline_codes(name VARCHAR(50))
 BEGIN
 	INSERT INTO discipline_codes(`Name`)
 	VALUES (name);
-END
+END;
 
 CREATE PROCEDURE get_discipline_code(id MEDIUMINT)
 BEGIN
 	SELECT * FROM discipline_codes WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_discipline_code(
 		id MEDIUMINT,
@@ -117,17 +118,17 @@ BEGIN
 	UPDATE discipline_codes
 	SET `Name` = name
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_discipline_code(id MEDIUMINT)
 BEGIN
 	DELETE FROM discipline_codes WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_discipline_code()
 BEGIN
 	DELETE FROM discipline_codes;
-END
+END;
 
 
 -- table: speciality_codes
@@ -136,12 +137,12 @@ CREATE PROCEDURE add_speciality_code(name VARCHAR(50))
 BEGIN
 	INSERT INTO speciality_codes(`Name`)
 	VALUES (name);
-END
+END;
 
 CREATE PROCEDURE get_speciality_code(id MEDIUMINT)
 BEGIN
 	SELECT * FROM speciality_codes WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_speciality_code(
 		id MEDIUMINT,
@@ -151,17 +152,17 @@ BEGIN
 	UPDATE speciality_codes
 	SET `Name` = name
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_speciality_code(id MEDIUMINT)
 BEGIN
 	DELETE FROM speciality_codes WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_speciality_code()
 BEGIN
 	DELETE FROM speciality_codes;
-END
+END;
 
 
 -- table: work_types
@@ -170,12 +171,12 @@ CREATE PROCEDURE add_work_type(name VARCHAR(50))
 BEGIN
 	INSERT INTO work_types(`Name`)
 	VALUES (name);
-END
+END;
 
 CREATE PROCEDURE get_work_type(id MEDIUMINT)
 BEGIN
 	SELECT * FROM work_types WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_work_type(
 		id MEDIUMINT,
@@ -185,17 +186,17 @@ BEGIN
 	UPDATE work_types
 	SET `Name` = name
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_work_type(id MEDIUMINT)
 BEGIN
 	DELETE FROM work_types WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_work_types()
 BEGIN
 	DELETE FROM work_types;
-END
+END;
 
 
 -- table: source_types
@@ -204,12 +205,12 @@ CREATE PROCEDURE add_source_type(name VARCHAR(50))
 BEGIN
 	INSERT INTO source_types(`Name`)
 	VALUES (name);
-END
+END;
 
 CREATE PROCEDURE get_source_type(id MEDIUMINT)
 BEGIN
 	SELECT * FROM source_types WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_source_type(
 		id MEDIUMINT,
@@ -219,17 +220,17 @@ BEGIN
 	UPDATE source_types
 	SET `Name` = name
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_source_type(id MEDIUMINT)
 BEGIN
 	DELETE FROM source_types WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_source_types()
 BEGIN
 	DELETE FROM source_types;
-END
+END;
 
 -- Tables with relations
 -- to other smaller tables
@@ -249,12 +250,12 @@ BEGIN
 	discipline,
 	speciality
 	);
-END
+END;
 
 CREATE PROCEDURE get_conformity(id MEDIUMINT)
 BEGIN
 	SELECT * FROM conformity WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_conformity(
 	id MEDIUMINT,
@@ -267,17 +268,17 @@ BEGIN
 	`Discipline` = discipline,
 	`Speciality` = speciality
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_conformity(id MEDIUMINT)
 BEGIN
 	DELETE FROM conformity WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_conformities()
 BEGIN
 	DELETE FROM conformity;
-END
+END;
 
 
 -- table: specialities
@@ -295,12 +296,12 @@ BEGIN
 	name,
 	code
 	);
-END
+END;
 
 CREATE PROCEDURE get_speciality(id MEDIUMINT)
 BEGIN
 	SELECT * FROM specialities WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_speciality(
 	id MEDIUMINT,
@@ -313,17 +314,17 @@ BEGIN
 	`Code` = code,
 	`Name` = name
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_speciality(id MEDIUMINT)
 BEGIN
 	DELETE FROM specialities WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_specialities()
 BEGIN
 	DELETE FROM specialities;
-END
+END;
 
 
 -- table: general_competetions
@@ -353,12 +354,12 @@ BEGIN
 	knowledge,
 	skills
 	);
-END
+END;
 
 CREATE PROCEDURE get_general_competetion(id MEDIUMINT)
 BEGIN
 	SELECT * FROM general_competetions WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_general_competetion(
 	id MEDIUMINT,
@@ -379,17 +380,17 @@ BEGIN
 	`Knowledge` = knowledge,
 	`Skills` = skills
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_general_competetion(id MEDIUMINT)
 BEGIN
 	DELETE FROM general_competetions WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_general_competetions()
 BEGIN
 	DELETE FROM general_competetions;
-END
+END;
 
 
 -- table: professional_competetions
@@ -425,12 +426,12 @@ BEGIN
 	skills,
 	experience 
 	);
-END
+END;
 
 CREATE PROCEDURE get_professional_competetion(id MEDIUMINT)
 BEGIN
 	SELECT * FROM professional_competetions WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_professional_competetion(
 	id MEDIUMINT,
@@ -455,17 +456,17 @@ BEGIN
 	`Skills` = skills,
 	`Experience` = experience
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_professional_competetion(id MEDIUMINT)
 BEGIN
 	DELETE FROM professional_competetions WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_professional_competetions()
 BEGIN
 	DELETE FROM professional_competetions;
-END
+END;
 
 
 -- table: competetions_mastering
@@ -489,12 +490,12 @@ BEGIN
 	general,
 	professional
 	);
-END
+END;
 
 CREATE PROCEDURE get_competetion_mastering(id MEDIUMINT)
 BEGIN
 	SELECT * FROM competetions_mastering WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_competetion_mastering(
 	id MEDIUMINT,
@@ -511,17 +512,17 @@ BEGIN
 	`General` = general,
 	`Professional` = professional
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_competetion_mastering(id MEDIUMINT)
 BEGIN
 	DELETE FROM competetions_mastering WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_competetions_mastering()
 BEGIN
 	DELETE FROM competetions_mastering;
-END
+END;
 
 
 -- table: disciplines
@@ -551,12 +552,12 @@ BEGIN
 	distance_course,
 	prepare_way
 	);
-END
+END;
 
 CREATE PROCEDURE get_discipline(id MEDIUMINT)
 BEGIN
 	SELECT * FROM disciplines WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_discipline(
 	id MEDIUMINT,
@@ -577,17 +578,17 @@ BEGIN
 	`DistanceCourse` = distance_course,
 	`PrepareWay` = prepare_way
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_discipline(id MEDIUMINT)
 BEGIN
 	DELETE FROM disciplines WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_disciplines()
 BEGIN
 	DELETE FROM disciplines;
-END
+END;
 
 
 -- table: theme_plan
@@ -611,12 +612,12 @@ BEGIN
 	name,
 	section_hours
 	);
-END
+END;
 
 CREATE PROCEDURE get_section(id MEDIUMINT)
 BEGIN
 	SELECT * FROM theme_plan WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_section(
 	id MEDIUMINT,
@@ -633,17 +634,17 @@ BEGIN
 	`Name` = name,
 	`Hours` = section_hours
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_section(id MEDIUMINT)
 BEGIN
 	DELETE FROM theme_plan WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_theme_plan()
 BEGIN
 	DELETE FROM theme_plan;
-END
+END;
 
 
 -- table: themes
@@ -670,12 +671,12 @@ BEGIN
 	name,
 	theme_hours
 	);
-END
+END;
 
 CREATE PROCEDURE get_theme(id MEDIUMINT)
 BEGIN
 	SELECT * FROM themes WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_theme(
 	id MEDIUMINT,
@@ -694,17 +695,17 @@ BEGIN
 	`Name` = name,
 	`Hours` = theme_hours
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_theme(id MEDIUMINT)
 BEGIN
 	DELETE FROM themes WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_themes()
 BEGIN
 	DELETE FROM themes;
-END
+END;
 
 
 -- table: works
@@ -722,12 +723,12 @@ BEGIN
 	theme,
 	work_type
 	);
-END
+END;
 
 CREATE PROCEDURE get_work(id MEDIUMINT)
 BEGIN
 	SELECT * FROM works WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_work(
 	id MEDIUMINT,
@@ -740,17 +741,17 @@ BEGIN
 	`Theme` = theme,
 	`Type` = work_type
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_work(id MEDIUMINT)
 BEGIN
 	DELETE FROM works WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_works()
 BEGIN
 	DELETE FROM works;
-END
+END;
 
 
 -- table: tasks
@@ -771,12 +772,12 @@ BEGIN
 	name,
 	task_hours
 	);
-END
+END;
 
 CREATE PROCEDURE get_task(id MEDIUMINT)
 BEGIN
 	SELECT * FROM tasks WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE set_task(
 	work_id MEDIUMINT,
@@ -790,14 +791,14 @@ BEGIN
 	`Name` = name,
 	`Hours` = task_hours
 	WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_task(id MEDIUMINT)
 BEGIN
 	DELETE FROM tasks WHERE `ID` = id;
-END
+END;
 
 CREATE PROCEDURE drop_all_tasks()
 BEGIN
 	DELETE FROM tasks;
-END
+END;
