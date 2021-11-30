47fdfca1754d5ecf46c0e84e8d26270c32d99660
Alexxx180 <alexx120702@gmail.com>
2021-11-30 19:25:04 +0300

    Created table views

--- CreateViews.sql
+++ CreateViews.sql
@@ -1,3 +1,96 @@
+-- Table views
+
+CREATE VIEW `view_meta_data` AS
+SELECT * FROM
+Meta_data;
+
+CREATE VIEW `view_meta_types` AS
+SELECT * FROM
+Meta_types;
+
+CREATE VIEW `view_conformity` AS
+SELECT * FROM
+conformity;
+
+CREATE VIEW `view_discipline_codes` AS
+SELECT * FROM
+discipline_codes;
+
+CREATE VIEW `view_disciplines` AS
+SELECT * FROM
+disciplines;
+
+CREATE VIEW `view_general_competetions` AS
+SELECT * FROM
+general_competetions;
+
+CREATE VIEW `view_general_mastering` AS
+SELECT * FROM
+general_mastering;
+
+CREATE VIEW `view_general_selection` AS
+SELECT * FROM
+general_selection;
+
+CREATE VIEW `view_hours` AS
+SELECT * FROM
+hours;
+
+CREATE VIEW `view_levels` AS
+SELECT * FROM
+levels;
+
+CREATE VIEW `view_professional_competetions` AS
+SELECT * FROM
+professional_competetions;
+
+CREATE VIEW `view_professional_mastering` AS
+SELECT * FROM
+professional_mastering;
+
+CREATE VIEW `view_professional_selection` AS
+SELECT * FROM
+professional_selection;
+
+CREATE VIEW `view_source_types` AS
+SELECT * FROM
+source_types;
+
+CREATE VIEW `view_sources` AS
+SELECT * FROM
+sources;
+
+CREATE VIEW `view_specialities` AS
+SELECT * FROM
+specialities;
+
+CREATE VIEW `view_speciality_codes` AS
+SELECT * FROM
+speciality_codes;
+
+CREATE VIEW `view_theme_plan` AS
+SELECT * FROM
+theme_plan;
+
+CREATE VIEW `view_themes` AS
+SELECT * FROM
+themes;
+
+CREATE VIEW `view_work_types` AS
+SELECT * FROM
+work_types;
+
+CREATE VIEW `view_works` AS
+SELECT * FROM
+works;
+
+CREATE VIEW `view_tasks` AS
+SELECT * FROM
+tasks;
+
+
+-- Custom views
+
 CREATE VIEW full_discipline_vision AS
 SELECT
 Disciplines.`ID` AS `No`,
