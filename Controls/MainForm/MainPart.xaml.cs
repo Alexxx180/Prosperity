9ac5613849050b71c22c7c0772b890aaf616d11f
Alexxx180 <alexx120702@gmail.com>
2022-01-22 20:28:23 +0300

    Alpha 1.35 | Increase code readability: unify fill table methods

--- Controls/MainForm/MainPart.xaml.cs
+++ Controls/MainForm/MainPart.xaml.cs
@@ -40,13 +40,18 @@ namespace Prosperity.Controls.MainForm
         public GlobalViewModel ViewModel { get; } = new GlobalViewModel();
         public LayoutMaster TableView;
 
+        private UserControl _tableHeader;
+
         public MainPart()
         {
             InitializeComponent();
             DataContext = ViewModel;
             TableView = TableViewControl.DataContext as LayoutMaster;
+            _tableHeader = DisciplineHeader;
             FillDisciplines();
         }
+
+        private string Before(string name) => "Ранее смотрели: " + name;
 
         private void Back(object sender, RoutedEventArgs e)
         {
@@ -65,18 +70,9 @@ namespace Prosperity.Controls.MainForm
 
         private void ResetHeaders(UserControl currentHeader)
         {
-            for (byte i = 0; i < CurrentHeaders.Children.Count; i++)
-            {
-                UserControl header = CurrentHeaders.Children[i] as UserControl;
-                header.Visibility = Visibility.Collapsed;
-            }
-            currentHeader.Visibility = Visibility.Visible;
-        }
-
-        private void ResetView(UserControl currentHeader)
-        {
-            ResetHeaders(currentHeader);
-            CurrentView.Children.Clear();
+            _tableHeader.Visibility = Visibility.Collapsed;
+            _tableHeader = currentHeader;
+            _tableHeader.Visibility = Visibility.Visible;
         }
 
         private void FillTables<T>(uint id, string name,
@@ -96,235 +92,210 @@ namespace Prosperity.Controls.MainForm
             FillTables<T>(id, name, markAction, transition, header, rows, additor);
         }
 
-        private void FillSecondaryTables(MarkBase.Action markAction, TransitionBase.Transition transition,
-            string name, uint id)
+        private void FillSecondaryTables<T>(uint id, string name,
+            MarkBase.Action markAction, TransitionBase.Transition transition,
+            UserControl header, List<string[]> rows, IAutoIndexing additor)
         {
             ViewModel.AddTransition(transition, name, id);
-
+            FillTables<T>(id, name, markAction, transition, header, rows, additor);
         }
 
         public void FillConformity(uint id = 0)
         {
-            ViewModel.CleanBuffer();
-            ViewModel.ChangeMarkMethod(Mark.Conformity, FillConformity, "Ранее смотрели: Соответствие - ID", id);
-            ResetView(ConformityHeader);
-            ConformityRow.AddElements(CurrentView, ViewModel.Data.Conformity, id);
+            FillPrimaryTables<ConformityRow>(id, Before("Соответствие - ID"), Mark.Conformity,
+                FillConformity, ConformityHeader, ViewModel.Data.Conformity, new ConformityRowAdditor());
         }
+
 
         public void FillSpecialities(uint id = 0)
         {
-            ViewModel.CleanBuffer();
-            ViewModel.ChangeMarkMethod(Mark.Speciality, FillSpecialities, "Ранее смотрели: Специальность - ID", id);
-            ResetView(SpecialityHeader);
-            SpecialityRow.AddElements(CurrentView, ViewModel.Data.Specialities, id);
+            FillPrimaryTables<SpecialityRow>(id, Before("Специальность - ID"), Mark.Speciality,
+                FillConformity, SpecialityHeader, ViewModel.Data.Specialities, new SpecialityRowAdditor());
         }
 
-        public void FillGeneralCompetetions(List<string[]> records)
+
+        public void FillGeneral(List<string[]> records, uint id,
+            string name, TransitionBase.Transition transition)
         {
-            ViewModel.ChangeMarkMethod(Mark.GeneralCompetetion);
-            ResetView(GeneralCompetetionHeader);
-            GeneralCompetetionRow.AddElements(CurrentView, records);
+            FillSecondaryTables<GeneralCompetetionRow>(id, name, Mark.GeneralCompetetion,
+                transition, GeneralCompetetionHeader, records, new GeneralCompetetionRowAdditor());
         }
 
         public void FillGeneralCompetetions(uint id)
         {
-            ViewModel.AddTransition(FillGeneralCompetetions, "Специальность - ID", id);
-            FillGeneralCompetetions(ViewModel.Data.GeneralCompetetions(id));
+            FillGeneral(ViewModel.Data.GeneralCompetetions(id),
+                id, "Специальность - ID", FillGeneralCompetetions);
         }
 
         public void FillGeneralFromMastering(uint id)
         {
-            ViewModel.AddTransition(FillGeneralFromMastering, "Дисциплина - ID", id);
-            FillGeneralCompetetions(ViewModel.Data.ConformityGeneralCompetetions(id));
+            FillGeneral(ViewModel.Data.ConformityGeneralCompetetions(id),
+                id, "Дисциплина - ID", FillGeneralFromMastering);
         }
 
-        public void FillProfessionalCompetetions(List<string[]> records)
+
+        public void FillProfessional(List<string[]> records, uint id,
+            string name, TransitionBase.Transition transition)
         {
-            ViewModel.ChangeMarkMethod(Mark.ProfessionalCompetetion);
-            ResetView(ProfessionalCompetetionHeader);
-            ProfessionalCompetetionRow.AddElements(CurrentView, records);
+            FillSecondaryTables<ProfessionalCompetetionRow>(id, name, Mark.ProfessionalCompetetion,
+                transition, ProfessionalCompetetionHeader, records, new ProfessionalCompetetionRowAdditor());
         }
 
         public void FillProfessionalCompetetions(uint id)
         {
-            ViewModel.AddTransition(FillProfessionalCompetetions, "Специальность - ID", id);
-            FillProfessionalCompetetions(ViewModel.Data.ProfessionalCompetetions(id));
+            FillProfessional(ViewModel.Data.ProfessionalCompetetions(id),
+                id, "Специальность - ID", FillProfessionalCompetetions);
         }
 
         public void FillProfessionalFromMastering(uint id)
         {
-            ViewModel.AddTransition(FillProfessionalFromMastering, "Дисциплина - ID", id);
-            FillGeneralCompetetions(ViewModel.Data.ConformityProfessionalCompetetions(id));
+            FillProfessional(ViewModel.Data.ConformityProfessionalCompetetions(id),
+                id, "Специальность - ID", FillProfessionalFromMastering);
         }
+
 
         public void FillDisciplines(uint id = 0)
         {
-            ViewModel.CleanBuffer();
-            ViewModel.ChangeMarkMethod(Mark.Discipline, FillDisciplines, "Ранее смотрели: Дисциплина - ID", id);
-            ResetView(DisciplineHeader);
-            TableView.AddElements<DisciplineRow>(ViewModel.Data.Disciplines, new DisciplineRowAdditor());
-            //DisciplineRow.AddElements(CurrentView, ViewModel.Data.Disciplines, id);
+            FillPrimaryTables<DisciplineRow>(id, Before("Дисциплина - ID"), Mark.Discipline,
+                FillDisciplines, DisciplineHeader, ViewModel.Data.Disciplines, new DisciplineRowAdditor());
         }
 
-        public void FillDisciplineGeneralCompetetions(List<string[]> rows)
+
+        public void FillDisciplineGeneral(List<string[]> records, uint id,
+            string name, TransitionBase.Transition transition)
         {
-            ViewModel.ChangeMarkMethod(Mark.GeneralMastering);
-            ResetView(GeneralMasteringHeader);
-            DisciplineGeneralMasteringRow.AddElements(CurrentView, rows);
+            FillSecondaryTables<DisciplineGeneralMasteringRow>(id, name, Mark.GeneralMastering,
+                transition, GeneralMasteringHeader, records, new DisciplineGeneralMasteringRowAdditor());
         }
 
         public void FillDisciplineGeneralCompetetions(uint id)
         {
-            ViewModel.AddTransition(FillDisciplineGeneralCompetetions, "Дисциплина - ID", id);
-            FillDisciplineGeneralCompetetions(ViewModel.Data.DisciplineGeneralMastering(id));
+            FillDisciplineGeneral(ViewModel.Data.DisciplineGeneralMastering(id),
+                id, "Дисциплина - ID", FillDisciplineGeneralCompetetions);
         }
 
         public void FillDisciplineGeneralFromMastering(uint id)
         {
-            ViewModel.AddTransition(FillDisciplineGeneralFromMastering, "Дисциплина - ID", id);
-            FillDisciplineGeneralCompetetions(ViewModel.Data.DisciplineGeneralMasteringByTheme(id));
+            FillDisciplineGeneral(ViewModel.Data.DisciplineGeneralMasteringByTheme(id),
+                id, "Дисциплина - ID", FillDisciplineGeneralFromMastering);
         }
 
-        public void FillDisciplineProfessionalCompetetions(List<string[]> rows)
+
+        public void FillDisciplineProfessional(List<string[]> records, uint id,
+            string name, TransitionBase.Transition transition)
         {
-            ViewModel.ChangeMarkMethod(Mark.ProfessionalMastering);
-            ResetView(ProfessionalMasteringHeader);
-            DisciplineProfessionalMasteringRow.AddElements(CurrentView, rows);
+            FillSecondaryTables<DisciplineProfessionalMasteringRow>(id, name, Mark.ProfessionalMastering,
+                transition, ProfessionalMasteringHeader, records, new DisciplineProfessionalMasteringRowAdditor());
         }
 
         public void FillDisciplineProfessionalCompetetions(uint id)
         {
-            ViewModel.AddTransition(FillDisciplineProfessionalCompetetions, "Дисциплина - ID", id);
-            FillDisciplineProfessionalCompetetions(ViewModel.Data.DisciplineProfessionalMastering(id));
+            FillDisciplineProfessional(ViewModel.Data.DisciplineProfessionalMastering(id),
+                id, "Дисциплина - ID", FillDisciplineProfessionalCompetetions);
         }
 
         public void FillDisciplineProfessionalFromMastering(uint id)
         {
-            ViewModel.AddTransition(FillDisciplineProfessionalFromMastering, "Дисциплина - ID", id);
-            FillDisciplineProfessionalCompetetions(ViewModel.Data.DisciplineProfessionalMasteringByTheme(id));
+            FillDisciplineProfessional(ViewModel.Data.DisciplineProfessionalMasteringByTheme(id),
+                id, "Дисциплина - ID", FillDisciplineProfessionalFromMastering);
         }
+
 
         public void FillSources(uint id)
         {
-            ViewModel.AddTransition(FillSources, "Дисциплина - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.Source);
-            ResetView(SourceHeader);
-            SourceRow.AddElements(CurrentView, ViewModel.Data.Sources(id));
+            FillSecondaryTables<SourceRow>(id, "Дисциплина - ID", Mark.ProfessionalMastering,
+                FillSources, SourceHeader, ViewModel.Data.Sources(id), new SourceRowAdditor());
         }
 
         public void FillMetaData(uint id)
         {
-            ViewModel.AddTransition(FillMetaData, "Дисциплина - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.MetaData);
-            ResetView(MetadataHeader);
-            MetaDataRow.AddElements(CurrentView, ViewModel.Data.MetaData(id));
+            FillSecondaryTables<MetaDataRow>(id, "Дисциплина - ID", Mark.MetaData, FillMetaData,
+                MetadataHeader, ViewModel.Data.MetaData(id), new MetaDataRowAdditor());
         }
 
         public void FillHours(uint id)
         {
-            ViewModel.AddTransition(FillHours, "Дисциплина - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.TotalHour);
-            ResetView(HoursHeader);
-            HoursRow.AddElements(CurrentView, ViewModel.Data.TotalHours(id));
+            FillSecondaryTables<HoursRow>(id, "Дисциплина - ID", Mark.TotalHour, FillHours,
+                HoursHeader, ViewModel.Data.TotalHours(id), new HoursRowAdditor());
         }
 
         public void FillTopics(uint id)
         {
-            ViewModel.AddTransition(FillTopics, "Дисциплина - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.Topic);
-            ResetView(ThemePlanHeader);
-            TopicRow.AddElements(CurrentView, ViewModel.Data.ThemePlan(id));
+            FillSecondaryTables<TopicRow>(id, "Дисциплина - ID", Mark.Topic, FillTopics,
+                ThemePlanHeader, ViewModel.Data.ThemePlan(id), new TopicRowAdditor());
         }
 
         public void FillThemes(uint id)
         {
-            ViewModel.AddTransition(FillThemes, "Раздел - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.Theme);
-            ResetView(ThemesHeader);
-            ThemeRow.AddElements(CurrentView, ViewModel.Data.Themes(id));
+            FillSecondaryTables<ThemeRow>(id, "Раздел - ID", Mark.Theme, FillThemes,
+                ThemesHeader, ViewModel.Data.Themes(id), new ThemeRowAdditor());
         }
 
         public void FillThemeGeneralCompetetions(uint id)
         {
-            ViewModel.AddTransition(FillThemeGeneralCompetetions, "Тема - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.GeneralSelection);
-            ResetView(GeneralSelectionHeader);
-            ThemeGeneralMasteringRow.AddElements(CurrentView, ViewModel.Data.ThemeGeneralMastering(id));
+            FillSecondaryTables<ThemeGeneralMasteringRow>(id, "Тема - ID", Mark.GeneralSelection,
+                FillThemeGeneralCompetetions, GeneralSelectionHeader, ViewModel.Data.ThemeGeneralMastering(id),
+                new ThemeGeneralMasteringRowAdditor());
         }
 
         public void FillThemeProfessionalCompetetions(uint id)
         {
-            ViewModel.AddTransition(FillThemeProfessionalCompetetions, "Тема - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.ProfessionalSelection);
-            ResetView(ProfessionalSelectionHeader);
-            ThemeProfessionalMasteringRow.AddElements(CurrentView, ViewModel.Data.ThemeProfessionalMastering(id));
+            FillSecondaryTables<ThemeProfessionalMasteringRow>(id, "Тема - ID", Mark.ProfessionalSelection,
+                FillThemeProfessionalCompetetions, ProfessionalSelectionHeader,
+                ViewModel.Data.ThemeProfessionalMastering(id), new ThemeProfessionalMasteringRowAdditor());
         }
 
         public void FillWorks(uint id)
         {
-            ViewModel.AddTransition(FillWorks, "Тема - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.Work);
-            ResetView(WorksHeader);
-            WorkRow.AddElements(CurrentView, ViewModel.Data.Works(id));
+            FillSecondaryTables<WorkRow>(id, "Тема - ID", Mark.Work, FillWorks,
+                WorksHeader, ViewModel.Data.Works(id), new WorkRowAdditor());
         }
 
         public void FillTasks(uint id)
         {
-            ViewModel.AddTransition(FillTasks, "Работа - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.Task);
-            ResetView(TasksHeader);
-            TaskRow.AddElements(CurrentView, ViewModel.Data.Tasks(id));
+            FillSecondaryTables<TaskRow>(id, "Работа - ID", Mark.Task, FillTasks,
+                TasksHeader, ViewModel.Data.Tasks(id), new TaskRowAdditor());
         }
 
 
         public void FillSpecialityCodes(uint id)
         {
-            ViewModel.AddTransition(FillSpecialities, "Ранее смотрели: Специальность - ID", id);
-            ViewModel.AddTransition(FillSpecialityCodes, "Ранее смотрели: Специальность - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.SpecialityCode);
-            ResetView(SpecialityCodesHeader);
-            SpecialityCodeRow.AddElements(CurrentView, ViewModel.Data.SpecialityCodes);
+            ViewModel.AddTransition(FillSpecialities, Before("Специальность - ID"), id);
+            FillSecondaryTables<SpecialityCodeRow>(id, Before("Специальность - ID"),
+                Mark.SpecialityCode, FillSpecialityCodes, SpecialityCodesHeader,
+                ViewModel.Data.SpecialityCodes, new SpecialityCodeRowAdditor());
         }
 
         public void FillDisciplineCodes(uint id)
         {
             ViewModel.AddTransition(FillDisciplines, "Ранее смотрели: Дисциплина - ID", id);
-            ViewModel.AddTransition(FillDisciplineCodes, "Ранее смотрели: Дисциплина - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.DisciplineCode);
-            ResetView(DisciplineCodesHeader);
-            DisciplineCodeRow.AddElements(CurrentView, ViewModel.Data.DisciplineCodes);
+            FillSecondaryTables<DisciplineCodeRow>(id, Before("Дисциплина - ID"),
+                Mark.DisciplineCode, FillDisciplineCodes, DisciplineCodesHeader,
+                ViewModel.Data.DisciplineCodes, new DisciplineCodeRowAdditor());
         }
 
         public void FillWorkTypes(uint id)
         {
-            ViewModel.AddTransition(FillWorkTypes, "Ранее смотрели: Работа/Час - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.WorkType);
-            ResetView(WorkTypesHeader);
-            WorkTypesRow.AddElements(CurrentView, ViewModel.Data.WorkTypes);
+            FillSecondaryTables<WorkTypesRow>(id, Before("Работа | Час - ID"), Mark.WorkType,
+                FillWorkTypes, WorkTypesHeader, ViewModel.Data.WorkTypes, new WorkTypesRowAdditor());
         }
 
         public void FillMetaTypes(uint id)
         {
-            ViewModel.AddTransition(FillMetaTypes, "Ранее смотрели: Метаданные - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.MetaType);
-            ResetView(MetaTypesHeader);
-            MetaTypeRow.AddElements(CurrentView, ViewModel.Data.MetaTypes);
+            FillSecondaryTables<MetaTypeRow>(id, Before("Метаданные - ID"), Mark.MetaType,
+                FillMetaTypes, MetaTypesHeader, ViewModel.Data.MetaTypes, new MetaTypeRowAdditor());
         }
 
         public void FillSourceTypes(uint id)
         {
-            ViewModel.AddTransition(FillSourceTypes, "Ранее смотрели: Источник - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.SourceType);
-            ResetView(SourceTypesHeader);
-            SourceTypeRow.AddElements(CurrentView, ViewModel.Data.SourceTypes);
+            FillSecondaryTables<SourceTypeRow>(id, Before("Источник - ID"), Mark.SourceType,
+                FillSourceTypes, SourceTypesHeader, ViewModel.Data.SourceTypes, new SourceTypeRowAdditor());
         }
 
         public void FillCompetetionLevels(uint id)
         {
-            ViewModel.AddTransition(FillCompetetionLevels, "Ранее смотрели: Тема - ID", id);
-            ViewModel.ChangeMarkMethod(Mark.Level);
-            ResetView(LevelsHeader);
-            LevelRow.AddElements(CurrentView, ViewModel.Data.Levels);
+            FillSecondaryTables<LevelRow>(id, Before("Тема - ID"), Mark.Level, FillCompetetionLevels,
+                LevelsHeader, ViewModel.Data.Levels, new LevelRowAdditor());
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
