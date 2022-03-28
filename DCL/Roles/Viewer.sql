3f061fcc0a9d5ef6560e51371e840eaa2f5592b7
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-03-28 16:06:00 +0300

    Linux environment update

--- DCL/Roles/Viewer.sql
+++ DCL/Roles/Viewer.sql
@@ -1,46 +1,46 @@
 -- view permissions
 
-GRANT EXECUTE ON PROCEDURE prosperity.get_professional_competetion
+GRANT EXECUTE ON PROCEDURE Prosperity.get_professional_competetion
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_specialities_full
+GRANT EXECUTE ON PROCEDURE Prosperity.get_specialities_full
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_speciality_general
+GRANT EXECUTE ON PROCEDURE Prosperity.get_speciality_general
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_speciality_professional
+GRANT EXECUTE ON PROCEDURE Prosperity.get_speciality_professional
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_disciplines_full
+GRANT EXECUTE ON PROCEDURE Prosperity.get_disciplines_full
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_speciality_disciplines
+GRANT EXECUTE ON PROCEDURE Prosperity.get_speciality_disciplines
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_discipline_total_hours
+GRANT EXECUTE ON PROCEDURE Prosperity.get_discipline_total_hours
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_theme_plan_by_discipline
+GRANT EXECUTE ON PROCEDURE Prosperity.get_theme_plan_by_discipline
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_themes_by_section
+GRANT EXECUTE ON PROCEDURE Prosperity.get_themes_by_section
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_work_by_theme
+GRANT EXECUTE ON PROCEDURE Prosperity.get_work_by_theme
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_work_types
+GRANT EXECUTE ON PROCEDURE Prosperity.get_work_types
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_task_by_work
+GRANT EXECUTE ON PROCEDURE Prosperity.get_task_by_work
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_discipline_meta_data
+GRANT EXECUTE ON PROCEDURE Prosperity.get_discipline_meta_data
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_meta_types
+GRANT EXECUTE ON PROCEDURE Prosperity.get_meta_types
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_discipline_sources
+GRANT EXECUTE ON PROCEDURE Prosperity.get_discipline_sources
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_source_type
+GRANT EXECUTE ON PROCEDURE Prosperity.get_source_type
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_source_types
+GRANT EXECUTE ON PROCEDURE Prosperity.get_source_types
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_discipline_general_mastering
+GRANT EXECUTE ON PROCEDURE Prosperity.get_discipline_general_mastering
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_discipline_professional_mastering
+GRANT EXECUTE ON PROCEDURE Prosperity.get_discipline_professional_mastering
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_theme_general_mastering_selection
+GRANT EXECUTE ON PROCEDURE Prosperity.get_theme_general_mastering_selection
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_theme_professional_mastering_selection
+GRANT EXECUTE ON PROCEDURE Prosperity.get_theme_professional_mastering_selection
 TO 'viewer';
-GRANT EXECUTE ON PROCEDURE prosperity.get_all_levels
+GRANT EXECUTE ON PROCEDURE Prosperity.get_all_levels
 TO 'viewer';
