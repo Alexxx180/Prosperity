b093d58aab55fdc8a99641bc36ec6f4ab088ea28
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-03-28 15:31:54 +0300

    Linux environment update

--- DML/Objects/Routines/Procedures/TablesModules/Drop/All.sql
+++ DML/Objects/Routines/Procedures/TablesModules/Drop/All.sql
@@ -1,4 +1,4 @@
-delimiter \;
+DELIMITER $$
 
 -- Tables with no relation
 -- to other smaller tables
@@ -7,50 +7,50 @@ delimiter \;
 
 CREATE PROCEDURE drop_all_levels()
 BEGIN
-	DELETE FROM levels;
-END;
+	DELETE FROM Levels;
+END$$
 
 -- table: hours
 
 CREATE PROCEDURE drop_all_hours()
 BEGIN
-	DELETE FROM hours;
-END;
+	DELETE FROM Hours;
+END$$
 
 -- table: discipline_codes
 
 CREATE PROCEDURE drop_all_discipline_code()
 BEGIN
-	DELETE FROM discipline_codes;
-END;
+	DELETE FROM Discipline_codes;
+END$$
 
 -- table: speciality_codes
 
 CREATE PROCEDURE drop_all_speciality_code()
 BEGIN
-	DELETE FROM speciality_codes;
-END;
+	DELETE FROM Speciality_codes;
+END$$
 
 -- table: work_types
 
 CREATE PROCEDURE drop_all_work_types()
 BEGIN
-	DELETE FROM work_types;
-END;
+	DELETE FROM Work_types;
+END$$
 
 -- table: source_types
 
 CREATE PROCEDURE drop_all_source_types()
 BEGIN
-	DELETE FROM source_types;
-END;
+	DELETE FROM Source_types;
+END$$
 
 -- table: meta types
 
 CREATE PROCEDURE drop_all_meta_type()
 BEGIN
-	DELETE FROM meta_types;
-END;
+	DELETE FROM Meta_types;
+END$$
 
 -- Tables with relations
 -- to other smaller tables
@@ -59,103 +59,103 @@ END;
 
 CREATE PROCEDURE drop_all_sources()
 BEGIN
-	DELETE FROM sources;
-END;
+	DELETE FROM Sources;
+END$$
 
 -- table: conformity
 
 CREATE PROCEDURE drop_all_conformities()
 BEGIN
-	DELETE FROM conformity;
-END;
+	DELETE FROM Conformity;
+END$$
 
 -- table: specialities
 
 CREATE PROCEDURE drop_all_specialities()
 BEGIN
-	DELETE FROM specialities;
-END;
+	DELETE FROM Specialities;
+END$$
 
 -- table: general_competetions
 
 CREATE PROCEDURE drop_all_general_competetions()
 BEGIN
-	DELETE FROM general_competetions;
-END;
+	DELETE FROM General_competetions;
+END$$
 
 -- table: professional_competetions
 
 CREATE PROCEDURE drop_all_professional_competetions()
 BEGIN
-	DELETE FROM professional_competetions;
-END;
+	DELETE FROM Professional_competetions;
+END$$
 
 -- table: general_mastering
 
 CREATE PROCEDURE drop_all_general_mastering()
 BEGIN
-	DELETE FROM general_mastering;
-END;
+	DELETE FROM General_mastering;
+END$$
 
 -- table: professional_mastering
 
 CREATE PROCEDURE drop_all_professional_mastering()
 BEGIN
-	DELETE FROM professional_mastering;
-END;
+	DELETE FROM Professional_mastering;
+END$$
 
 -- table: general_selection
 
 CREATE PROCEDURE drop_all_general_selection()
 BEGIN
-	DELETE FROM general_selection;
-END;
+	DELETE FROM General_selection;
+END$$
 
 -- table: professional_selection
 
 CREATE PROCEDURE drop_all_professional_selection()
 BEGIN
-	DELETE FROM professional_selection;
-END;
+	DELETE FROM Professional_selection;
+END$$
 
 -- table: disciplines
 
 CREATE PROCEDURE drop_all_disciplines()
 BEGIN
-	DELETE FROM disciplines;
-END;
+	DELETE FROM Disciplines;
+END$$
 
 -- table: theme_plan
 
 CREATE PROCEDURE drop_all_theme_plan()
 BEGIN
-	DELETE FROM theme_plan;
-END;
+	DELETE FROM Theme_plan;
+END$$
 
 -- table: themes
 
 CREATE PROCEDURE drop_all_themes()
 BEGIN
-	DELETE FROM themes;
-END;
+	DELETE FROM Themes;
+END$$
 
 -- table: works
 
 CREATE PROCEDURE drop_all_works()
 BEGIN
-	DELETE FROM works;
-END;
+	DELETE FROM Works;
+END$$
 
 -- table: tasks
 
 CREATE PROCEDURE drop_all_tasks()
 BEGIN
-	DELETE FROM tasks;
-END;
+	DELETE FROM Tasks;
+END$$
 
 -- table: meta data
 
 CREATE PROCEDURE drop_all_meta_data()
 BEGIN
 	DELETE FROM Meta_data;
-END;
+END$$
