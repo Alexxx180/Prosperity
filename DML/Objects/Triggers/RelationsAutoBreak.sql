5443f798f0c40ab72ad021fd246c95376e4d15d8
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-03-28 16:03:54 +0300

    Linux environment update

--- DML/Objects/Triggers/RelationsAutoBreak.sql
+++ DML/Objects/Triggers/RelationsAutoBreak.sql
@@ -1,18 +1,16 @@
+DELIMITER $$
+
 -- linked tables delete rows
 
-delimiter \;
-
 CREATE TRIGGER delete_work_linked
-BEFORE DELETE ON works FOR EACH ROW
+BEFORE DELETE ON Works FOR EACH ROW
 BEGIN
 	DELETE FROM Tasks
 	WHERE `Work` = OLD.`ID`;
-END;
-
-delimiter \;
+END$$
 
 CREATE TRIGGER delete_theme_linked
-BEFORE DELETE ON themes FOR EACH ROW
+BEFORE DELETE ON Themes FOR EACH ROW
 BEGIN
 	DELETE FROM Works
 	WHERE `Theme` = OLD.`ID`;
@@ -22,17 +20,17 @@ BEGIN
 	
 	DELETE FROM Professional_selection
 	WHERE `Theme` = OLD.`ID`;
-END;
+END$$
 
 CREATE TRIGGER delete_topic_linked
-BEFORE DELETE ON theme_plan FOR EACH ROW
+BEFORE DELETE ON Theme_plan FOR EACH ROW
 BEGIN
 	DELETE FROM Themes
 	WHERE `Topic` = OLD.ID;
-END;
+END$$
 
 CREATE TRIGGER delete_discipline_linked
-BEFORE DELETE ON disciplines FOR EACH ROW
+BEFORE DELETE ON Disciplines FOR EACH ROW
 BEGIN
 	DELETE FROM Conformity
 	WHERE `Discipline` = OLD.ID;
@@ -54,38 +52,38 @@ BEGIN
 	
 	DELETE FROM Professional_mastering
 	WHERE `Discipline` = OLD.ID;
-END;
+END$$
 
 CREATE TRIGGER delete_general_mastering_linked
-BEFORE DELETE ON general_mastering FOR EACH ROW
+BEFORE DELETE ON General_mastering FOR EACH ROW
 BEGIN
 	DELETE FROM General_selection
 	WHERE `Mastering` = OLD.ID;
-END;
+END$$
 
 CREATE TRIGGER delete_professional_mastering_linked
-BEFORE DELETE ON professional_mastering FOR EACH ROW
+BEFORE DELETE ON Professional_mastering FOR EACH ROW
 BEGIN
 	DELETE FROM Professional_selection
 	WHERE `Mastering` = OLD.ID;
-END;
+END$$
 
 CREATE TRIGGER delete_general_competetion_linked
-BEFORE DELETE ON general_competetions FOR EACH ROW
+BEFORE DELETE ON General_competetions FOR EACH ROW
 BEGIN
 	DELETE FROM General_mastering
 	WHERE `Mastering` = OLD.ID;
-END;
+END$$
 
 CREATE TRIGGER delete_professional_competetion_linked
-BEFORE DELETE ON professional_competetions FOR EACH ROW
+BEFORE DELETE ON Professional_competetions FOR EACH ROW
 BEGIN
 	DELETE FROM Professional_mastering
 	WHERE `Mastering` = OLD.ID;
-END;
+END$$
 
 CREATE TRIGGER delete_speciality_linked
-BEFORE DELETE ON specialities FOR EACH ROW
+BEFORE DELETE ON Specialities FOR EACH ROW
 BEGIN
 	DELETE FROM Conformity
 	WHERE `Speciality` = OLD.ID;
@@ -95,4 +93,4 @@ BEGIN
 	
 	DELETE FROM Professional_competetions
 	WHERE `Speciality` = OLD.ID;
-END;
+END$$
