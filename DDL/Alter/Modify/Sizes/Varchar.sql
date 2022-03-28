0c0bcdb3ff3fef7a332828e747f03703e22d837b
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-03-28 11:56:31 +0300

    Linux environment update

--- DDL/Alter/Modify/Sizes/Varchar.sql
+++ DDL/Alter/Modify/Sizes/Varchar.sql
@@ -1,6 +1,6 @@
 -- This can be handful to increase varchar column sizes manually
 
-CALL seek_for_column_type('varchar');
+-- CALL seek_for_column_type('varchar');
 
 -- MySQL doesn't have means to optimize that, 
 -- so we use only one column per query
