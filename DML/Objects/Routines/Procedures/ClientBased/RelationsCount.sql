c7e4d3e3591a703cc2060ed152e016dce2567c17
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-03-28 15:50:32 +0300

    Linux environment update

--- DML/Objects/Routines/Procedures/ClientBased/RelationsCount.sql
+++ DML/Objects/Routines/Procedures/ClientBased/RelationsCount.sql
@@ -1,4 +1,4 @@
-delimiter \;
+DELIMITER $$
 
 CREATE PROCEDURE get_work_type_linked(type_id INT UNSIGNED)
 BEGIN
@@ -15,39 +15,39 @@ BEGIN
 	    FROM Works
 	    WHERE `Type` = type_id
 	) AS `CountResults`;
-END;
+END$$
 
 CREATE PROCEDURE get_level_linked(level_id INT UNSIGNED)
 BEGIN
 	SELECT COUNT(`ID`) AS `Count`
 	FROM Themes
 	WHERE `Level` = level_id;
-END;
+END$$
 
 CREATE PROCEDURE get_meta_type_linked(type_id INT UNSIGNED)
 BEGIN
 	SELECT COUNT(`ID`) AS `Count`
 	FROM Meta_data
 	WHERE `Type` = type_id;
-END;
+END$$
 
 CREATE PROCEDURE get_source_type_linked(type_id INT UNSIGNED)
 BEGIN
 	SELECT COUNT(`ID`) AS `Count`
 	FROM Sources
 	WHERE `Type` = type_id;
-END;
+END$$
 
 CREATE PROCEDURE get_discipline_code_linked(code_id INT UNSIGNED)
 BEGIN
 	SELECT COUNT(`ID`) AS `Count`
 	FROM Disciplines
 	WHERE `Code` = code_id;
-END;
+END$$
 
 CREATE PROCEDURE get_speciality_code_linked(code_id INT UNSIGNED)
 BEGIN
 	SELECT COUNT(`ID`) AS `Count`
 	FROM Specialities
 	WHERE `Code` = code_id;
-END;
+END$$
