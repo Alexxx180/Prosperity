38782124d2f38c1908d3b98267d15e43ddfab4a8
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-04-04 22:10:34 +0200

    Linux environment update

--- DML/Objects/Routines/Procedures/PreparedStatements/Redactors.sql
+++ DML/Objects/Routines/Procedures/PreparedStatements/Redactors.sql
@@ -12,26 +12,30 @@ BEGIN
 	WHERE `User` = redactor_login;
 END$$
 
+-- Maria DB:
+-- change " 'SET DEFAULT ROLE ''redactor'' TO ''' "
+-- to " 'SET DEFAULT ROLE ''redactor'' FOR ''' "
+
 CREATE PROCEDURE add_redactor(
 	IN redactor_login CHAR(32),
 	IN redactor_host CHAR(60),
 	IN redactor_pass VARCHAR(255))
 BEGIN
-	SET @s1 = CONCAT('CREATE USER ',
-	redactor_login, '@', redactor_host,
-	' IDENTIFIED BY \'', redactor_pass, '\';');
+	SET @s1 = CONCAT('CREATE USER ''',
+	redactor_login, '''@''', redactor_host,
+	''' IDENTIFIED BY ''', redactor_pass, ''';');
 	PREPARE stmt FROM @s1;
 	EXECUTE stmt;
 	DEALLOCATE PREPARE stmt;
 	
-	SET @s2 = CONCAT('GRANT \'redactor\' TO ',
-	redactor_login, '@', redactor_host, ';');
+	SET @s2 = CONCAT('GRANT ''redactor'' TO ''',
+	redactor_login, '''@''', redactor_host, ''';');
 	PREPARE stmt FROM @s2;
 	EXECUTE stmt;
 	DEALLOCATE PREPARE stmt;
 	
-	SET @s3 = CONCAT('SET DEFAULT ROLE \'redactor\' TO ',
-	redactor_login, '@', redactor_host, ';');
+	SET @s3 = CONCAT('SET DEFAULT ROLE ''redactor'' TO ''',
+	redactor_login, '''@''', redactor_host, ''';');
 	PREPARE stmt FROM @s3;
 	EXECUTE stmt;
 	DEALLOCATE PREPARE stmt;
@@ -48,8 +52,8 @@ CREATE PROCEDURE drop_redactor(
 	IN redactor_login CHAR(32),
 	IN redactor_host CHAR(60))
 BEGIN
-	SET @s = CONCAT('DROP USER IF EXISTS ',
-	redactor_login, '@', redactor_host, ';');
+	SET @s = CONCAT('DROP USER IF EXISTS \'',
+	redactor_login, '\'@\'', redactor_host, '\';');
 	PREPARE stmt FROM @s;
 	EXECUTE stmt;
 	DEALLOCATE PREPARE stmt;
