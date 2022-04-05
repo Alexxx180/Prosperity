5976810154fa2a5b8d05a77ad696ba6e1f5069fc
Alexxx180 <alexx120702@gmail.com>
2022-04-05 22:17:06 +0300

    Beta 1.006 | Critical bug fixed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,10 +23,11 @@ namespace Prosperity
 
         public MainWindow()
         {
-            TotalViewModel = new GlobalViewModel();
+            GlobalViewModel test = new GlobalViewModel();
 
-            if (TotalViewModel.Connect())
+            if (test.Connect())
             {
+                TotalViewModel = test;
                 ActivateAdmin();
             }
             else
